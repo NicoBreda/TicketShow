55a13a9afbe31da7bf43d8f68a0020ef40306482
nicobreda <nicolasbreda@hotmail.com>
2022-04-17 03:26:58 -0300

    entrega final

---
+++ src/js/stock.js
@@ -0,0 +1,9 @@
+//entradas a la venta
+let stockProductos = [
+    {id:1, nombre: "David Guilmour-campo", cantidad: 1, imagen: 'src/multimedia/Guilmour.png', precio: "5000"},
+    {id:2, nombre: "U2-campo", cantidad: 1, imagen: "src/multimedia/U2.png", precio: "4000"},
+    {id:3, nombre: "Metronomy-campo", cantidad: 1, imagen: "src/multimedia/Metronomy.png", precio: "3000"},
+    {id:4, nombre: "David Guilmour-platea", cantidad: 1, imagen: "src/multimedia/Guilmour.png", precio: "9000"},
+    {id:5, nombre: "U2-platea", cantidad: 1, imagen: "src/multimedia/U2.png", precio: "7000"},
+    {id:6, nombre: "Metronomy-platea", cantidad: 1, imagen: "src/multimedia/Metronomy.png", precio: "5000"}];
+ 
